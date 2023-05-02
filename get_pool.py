6900ea67394bd9a9c59b659eb1db49553e3d979e
unknown <errfig22@gmail.com>
2023-05-02 15:46:43 +0100

    added 1337fil

--- get_pool.py
+++ get_pool.py
@@ -1,9 +1,10 @@
 import requests
 from time import sleep
 dict = {'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'}
-for i in range(0,5):
+# check every 5min in 24 hours
+for i in range(0,288):
     print("starting again")
-    r = requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=JoeSan76228343&count=1",headers=dict)
+    r = requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=1337fil&count=1",headers=dict)
     if  "piscine" in r.json()[0]['text'] or "pool" in r.json()[0]['text']:
         requests.get("https://api.telegram.org/bot6156374149:AAEWgpCby2jcVGFcDWIQQZphK8cQaGrFiWE/sendMessage?chat_id=-1001977172268&text=🔥⚡🔥 P001 I5 L04DIN9 !!! 🔥⚡🔥 \n 🍀 good Luck 🍀")
     print("waiting for next 5 min ...")
