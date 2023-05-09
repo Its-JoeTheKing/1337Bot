bbea7f2fd8caf3137a9dd313437c2bcc1f6376f1
unknown <errfig22@gmail.com>
2023-05-09 22:42:58 +0100

    lowercase fixed

--- get_pool.py
+++ get_pool.py
@@ -5,7 +5,8 @@ dict = {'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E
 for i in range(0,288):
     print("starting again")
     r = requests.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=1337fil&count=1",headers=dict)
-    if  "piscine" in r.json()[0]['text'] or "pool" in r.json()[0]['text']:
+    tweet = r.json()[0]['text'].lower()
+    if  "piscine" in tweet or "pool" in tweet:
         requests.get("https://api.telegram.org/bot6156374149:AAEWgpCby2jcVGFcDWIQQZphK8cQaGrFiWE/sendMessage?chat_id=-1001977172268&text=🔥⚡🔥 P001 I5 L04DIN9 !!! 🔥⚡🔥 \n 🍀 good Luck 🍀")
     print("waiting for next 5 min ...")
     sleep(300)
